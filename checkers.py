78ff7245502b98043eff0a2e021998a5957b0817
Lavi <lavifb@gmail.com>
2015-01-02 17:26:14 -0500

    some cleanup and function defining

--- checkers.py
+++ checkers.py
@@ -2,34 +2,36 @@
 board = [[0 for i in xrange(8)] for i in xrange(8)]
 
 
-for m in xrange(8):
-	if m%2 == 0:
-		board[0][m] = 1
-		board[2][m] = 1
-		board[6][m] = 2
-	else:
-		board[1][m] = 1
-		board[5][m] = 
-		board[7][m] = 2
-
-
-
+def setupBoard():
+	for m in xrange(8):
+		if m%2 == 0:
+			board[0][m] = 1
+			board[2][m] = 1
+			board[6][m] = 2
+		else:
+			board[1][m] = 1
+			board[5][m] = 2
+			board[7][m] = 2
 
 def printBoard():
-	for row in board:
+	for n in xrange(8):
 		pRow = ''
-		for spot in row:
-			if spot == 0:
-				pRow += '.'
-			elif spot == 1:
-				pRow += 'X'
-			elif spot == 2:
-				pRow += 'O'
+		for m in xrange(8):
+			if board[n][m] == 0:
+				if (n+m)%2 == 0:
+					pRow += '. '
+				else:
+					pRow += '  '
+			elif board[n][m] == 1:
+				pRow += 'X '
+			elif board[n][m] == 2:
+				pRow += 'O '
 		print pRow
 
 
 
-for row in board:
-	print row
+# for row in board:
+	# print row
 
+setupBoard()
 printBoard()
