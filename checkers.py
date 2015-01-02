f79407b3c91850281510b31386c7dfa6ddca754e
Lavi <lavifb@gmail.com>
2015-01-02 17:21:22 -0500

    added print board

--- checkers.py
+++ checkers.py
@@ -1,5 +1,24 @@
 
 board = [[0]*8]*8
 
+
+
+
+def printBoard():
+	for row in board:
+		pRow = ''
+		for spot in row:
+			if spot == 0:
+				pRow += '.'
+			elif spot == 1:
+				pRow += 'X'
+			elif spot == 2:
+				pRow += 'O'
+		print pRow
+
+
+
 for row in board:
 	print row
+
+printBoard()
