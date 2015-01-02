5a8745917367a438bd5dd36deda3becf10616271
Lavi <lavifb@gmail.com>
2015-01-02 18:11:10 -0500

    added piece moving

--- checkers.py
+++ checkers.py
@@ -53,7 +53,8 @@ def getInput(player):
 	n, m = move/1000%10, move/100%10
 	a, b = move/10%10, move%10
 	if isLegalMove(player,n,m,a,b):
-		pass
+		board[n][m] = 0
+		board[a][b] = player
 		newPlayer = player+1
 		if newPlayer > 2:
 			newPlayer -= 2
